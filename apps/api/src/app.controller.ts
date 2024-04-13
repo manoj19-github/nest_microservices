9bf2d1f9b0516e6da820dc424b42d759e45f63da
manoj santra <santramanoj1997@gmail.com>
2024-04-13 12:57:21 +0530

    first commit

---
+++ apps/api/src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    @Inject('AUTH_SERVICE') private authService: ClientProxy,
+  ) {}
+  
+}
